f290afa9eac276686c551f8aca7653944367b8df
EC2 Default User <ec2-user@ip-10-150-4-226.us-east-2.compute.internal>
2022-10-16 22:06:55 +0000

    first push

---
+++ config/app_config.py
@@ -0,0 +1,91 @@
+"""
+    The moudle defined the class that loads the environment variables:
+     - the database values that compose the connection string.
+     - the aws connection access and processing bucket.
+"""
+
+from os import environ
+from typing import get_type_hints, Union
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class AppConfigError(Exception):
+    """
+    Throw Exception when any env variables are missing
+    """
+
+
+def _parse_bool(val: Union[str, bool]) -> bool:  # pylint: disable=E1136
+    return val if isinstance(val, bool) else val.lower() in ['true', 'yes', 1]
+
+
+class AppConfig:
+    """
+        AppConfig class with required fields, default values, type checking, and typecasting for int and bool values
+    """
+
+    DEBUG: bool = False
+    VERSION: str = '1.0.0'
+    DATABASE_TENANT_HOSTNAME: str
+    DATABASE_TENANT_PORT: int
+    DATABASE_TENANT_DBNAME: str
+    DATABASE_TENANT_USERNAME: str
+    DATABASE_TENANT_PASSWORD: str
+    DATABASE_TENANT_PEM_PATH: str
+
+    SSH_PKEY_PATH:str
+    SSH_HOST:str
+    SSH_PORT:str
+    SSH_USERNAME:str
+
+    DOCUMENTDB_USERNAME:str
+    DOCUMENTDB_PASSWORD:str
+    DOCUMENTDB_DB:str
+    DOCUMENTDB_HOST:str
+    DOCUMENTDB_PORT:int
+
+
+
+    """
+        Map environment variables to class fields according to these rules:
+        - Field won't be parsed unless it has a type annotation
+        - Field will be skipped if not in all caps
+        - Class field and environment variable name are the same
+    """
+
+    def __init__(self):
+        for field in self.__annotations__:
+            if not field.isupper():
+                continue
+
+            if self._is_missing(field):
+                raise AppConfigError(f'The {field} field is required.')
+
+            try:
+                value = self._get_env_value(field)
+                self.__setattr__(field, value)
+            except ValueError as value_error:
+                raise AppConfigError(
+                    f'Unable to cast value of "{environ[field]}" to type \
+                         "{get_type_hints(AppConfig)[field]}" for "{field}" field') from value_error
+
+    def _is_missing(self, field: str) -> bool:
+        return getattr(self, field, None) is None and environ.get(field) is None
+
+    def _get_env_value(self, field: str) -> Union[str, bool]:
+        if get_type_hints(AppConfig)[field] == bool:
+            result = _parse_bool(
+                environ.get(field, getattr(self, field, None)))
+        else:
+            result = get_type_hints(AppConfig)[field](
+                environ.get(field, getattr(self, field, None)))
+
+        return result
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+
+Config = AppConfig()
---
+++ database/database_client.py
@@ -0,0 +1,244 @@
+"""
+This module contains all the classes and functions related to database handling
+"""
+
+# from urllib import parse, quote
+import psycopg2
+import pymongo
+import urllib
+import pandas as pd
+from typing import List
+from config.app_config import Config
+from sshtunnel import SSHTunnelForwarder
+from decimal import Decimal
+from bson.decimal128 import Decimal128
+
+def query_rds(sql,data_science, conn_list):
+    """
+
+    Parameters
+        ----------
+        data_science : bool, optional
+            If the code is being used outside the VPC, then we need to do a tunneling. Set this parameter to True
+
+    """
+
+    if data_science:
+        
+        try:
+
+            # Create an SSH tunnel
+
+            with SSHTunnelForwarder(
+                Config.SSH_HOST,
+                ssh_username=Config.SSH_USERNAME,
+                ssh_pkey=Config.SSH_PKEY_PATH,
+                remote_bind_address=(Config.DATABASE_TENANT_HOSTNAME, 7678)) as server:
+                
+                print("****SSH Tunnel Established****") 
+
+                conn = psycopg2.connect(
+                    
+
+                    user=conn_list[3],
+                    password = conn_list[4],
+                    host = "localhost",  
+                    port= server.local_bind_port,
+                    database = conn_list[2]
+                    )
+
+
+                # Get a database cursor
+                cur = conn.cursor()
+
+                # Execute SQL
+                cur.execute(sql)
+                # Get the result
+                result = cur.fetchall()
+
+
+                conn.close()
+            return result
+
+        except Exception as ex:
+            print(ex)
+
+    else:
+        try:
+            conn = psycopg2.connect(
+                        user=conn_list[3],
+                        password = conn_list[4],
+                        host = conn_list[0], 
+                        port= conn_list[1],
+                        database = conn_list[2]
+                        )
+
+
+                                # Get a database cursor
+            cur = conn.cursor()
+
+            # Execute SQL
+            cur.execute(sql)
+            # Get the result
+            result = cur.fetchall()
+
+            conn.close()
+            return result
+
+        except Exception as ex:
+            print(ex)
+
+    return None
+
+
+def query_iqvia(lista_eans):
+
+    user = Config.DOCUMENTDB_USERNAME
+    password = urllib.parse.quote_plus(Config.DOCUMENTDB_PASSWORD)
+    db = Config.DOCUMENTDB_DB
+    host = Config.DOCUMENTDB_HOST
+    port = Config.DOCUMENTDB_PORT
+    pem_path = Config.DATABASE_TENANT_PEM_PATH
+
+    ##Create a MongoDB client, open a connection to Amazon DocumentDB as a replica set and specify the read preference as secondary preferred
+    client = pymongo.MongoClient(f'mongodb://{user}:{password}@{host}:{port}/?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false') 
+
+    ##Specify the database to be used
+    db = client[db]
+
+    # Collection name
+    col = db["iqvia"]
+
+
+    lista_info = []
+    for ean in lista_eans:
+        filter = {'ean': ean}
+        cursor = col.find(filter)
+        for doc in cursor:
+            lista_info.append(doc)
+
+    df_iqvia = pd.DataFrame(lista_info)
+    # print('##################################')
+    # print(' ')
+    # print(df_iqvia.columns)
+    # print(' ')
+    # print('##################################')
+    
+    
+    return df_iqvia[['ean','classe4','forma3','fcc']]
+
+
+
+def convert_decimal(dict_item):
+    # This function iterates a dictionary looking for types of Decimal and converts them to Decimal128
+    # Embedded dictionaries and lists are called recursively.
+    if dict_item is None: return None
+
+    for k, v in list(dict_item.items()):
+        if isinstance(v, dict):
+            convert_decimal(v)
+        elif isinstance(v, list):
+            for l in v:
+                convert_decimal(l)
+        elif isinstance(v, Decimal):
+            dict_item[k] = Decimal128(str(v))
+
+    return dict_item
+
+
+def inserir_document_db(df_de_transacoes):
+    # print(df_de_transacoes)
+    user = Config.DOCUMENTDB_USERNAME
+    password = urllib.parse.quote_plus(Config.DOCUMENTDB_PASSWORD)
+    db = Config.DOCUMENTDB_DB
+    host = Config.DOCUMENTDB_HOST
+    port = Config.DOCUMENTDB_PORT
+    pem_path = Config.DATABASE_TENANT_PEM_PATH
+
+
+    ##Create a MongoDB client, open a connection to Amazon DocumentDB as a replica set and specify the read preference as secondary preferred
+    client = pymongo.MongoClient(f'mongodb://{user}:{password}@{host}:{port}/?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false') 
+    # print('ok1', flush = True, end = '\n')
+    ##Specify the database to be used
+    db = client[db]
+
+    # Collection name
+    col = db["transacao"]
+    count = 0
+
+    list_of_itens = []
+
+    # print('Removendo', flush = True, end = '\n')
+    db.transacao.delete_many({})
+    # print('Inserindo', flush = True, end = '\n')
+
+    info = df_de_transacoes.to_dict(orient = 'records')
+    
+    return col.insert_many(info)
+
+class DatabaseClient:
+    """
+        DatabaseClient abstracts the database connecion
+        and query execution procedure.
+
+        Parameters
+        ----------
+        data_science : bool, optional
+            If the code is being used outside the VPC, then we need to do a tunneling. Set this parameter to True
+
+        
+    ...
+
+    Attributes
+    ----------
+    connetion : psycopg2.connect
+        a formatted string to print out what the animal says
+    name : str
+        the name of the animal
+    sound : str
+        the sound that the animal makes
+    num_legs : int
+        the number of legs the animal has (default 4)
+
+    Methods
+    -------
+    says(sound=None)
+        Prints the animals name and what sound it makes
+
+    """
+
+    def __init__(self,data_science = False):
+        # self.connection = create_connection(data_science)
+        self.ds = data_science
+
+    def get_connection(self) -> psycopg2.connect:
+        """
+            Return the current connection to the database.
+        """
+        return self.connection
+
+    def query_to_df(self, sql: str, lista_conn:list) -> pd.DataFrame:
+        """
+            Execute a select query on a database and convert the
+            result to a pandas dataframe.
+        """
+
+        try:
+            result= query_rds(sql, self.ds, lista_conn)
+
+
+
+            return result
+            
+        except Exception as ex:
+            print(ex)
+
+
+    def insert_documentdb(self, df):
+        return inserir_document_db(df)
+
+    def iqvia_info(self, list_eans):
+        return query_iqvia(list_eans)
+
+
+
